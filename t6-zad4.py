ecac45d8261ac2b3afc1c01b15dde46d3cddd827
ADIK03 <61296015+ADIK03@users.noreply.github.com>
2023-09-20 22:08:11 +0200

    Update t6-zad4.py

--- t6-zad4.py
+++ t6-zad4.py
@@ -5,9 +5,20 @@
     litery, które pojawiły się w tekście.
 
 '''
-
+# pętla for
 s = input("Podaj łańcuch znaków: ")
 
 for i in range(len(s)):
     if s[i] >= 'A' and s[i] <= 'Z':
         print(s[i], end="")
+
+
+# pętla while
+s = input("Podaj łańcuch znaków: ")
+
+i = 0
+while i < len(s):
+    if s[i] >= 'A' and s[i] <= 'Z':
+        print(s[i], end="")
+    i += 1
+
