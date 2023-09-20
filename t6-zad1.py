6333d18d08c4406bd1f09afcd5f985e13eaa7364
ADIK03 <61296015+ADIK03@users.noreply.github.com>
2023-09-20 22:05:05 +0200

    Update t6-zad1.py

--- t6-zad1.py
+++ t6-zad1.py
@@ -3,17 +3,15 @@
     Napisz program, który wczyta z klawiatury nazwę miejscowości (bez spacji) 
     i wypisze co drugą jej literę. Litery powinny być rozdzielone spacjami.
 '''
-
+# pętla while
 miejscowosc = input("Podaj nazwę miejscowości: ")
 
-# pętla while
 i = 1
 while i < len(miejscowosc):
     print(miejscowosc[i], end=" ")
     i = i + 2
 
 # pętla for
-
 miejscowosc = input("Podaj nazwę miejscowości: ")
 
 for i in range(1, len(miejscowosc), 2):
